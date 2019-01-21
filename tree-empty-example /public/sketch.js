2f6d9dfe0ca899b3b6c701411fa34620405c1c05
Maryam Shitu <maryamshitu@gmail.com>
2019-01-20 18:46:07 -0800

    background and flocking added

--- tree-empty-example /public/sketch.js
+++ tree-empty-example /public/sketch.js
@@ -1,4 +1,6 @@
+//// adding tree test
 ///////////variable declarations////////////////////////
+
 let socket; // socket lib allows one user to draw and another see the drawing
 // tree features
 let s = 0; // scale
@@ -28,19 +30,21 @@ let currentWeather;
 // wind
 let wind;
 let windPosition; // position of wind objects
-let angleW;
-//   wind = p5.Vector.fromAngle(angle);
+let windAngle;
 
-
-// sliders
-let angleSlider, coefSlider, lSlider, branchesSlider, stepsSlider;
-
-/// collects rain drops
+/// rain
 let nDrops = 1000;
 let drops = [];
 
 // flock
 let flock;
+
+//Mountains
+let mountains;
+let time;
+
+// text
+let graphics;
 
 function preload() {
   weatherAsk(); // calls api and gets weather
@@ -49,6 +53,7 @@ function preload() {
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   textSize(20);
+
 
   // create raindrops
   for (i = 0; i < nDrops; i++) {
@@ -65,36 +70,46 @@ function setup() {
   // Add an initial set of boids into the system
   if (currentWindSpeed) {
     for (var i = 0; i < 100; i++) {
-      var b = new Boid(width/2,height/2, wind, windPosition);
+      var b = new Boid(width/2,height/2);
       flock.addBoid(b);
     }
   }
+
+  time = random(1);
+  mountains = new Mountains(height - 100, height - 10, time);
+
 }
 
 
 function draw() {
   background(255);
   count++;
-
   showWeather();
 
 //  start rain
+  push();
   if (currentWeather === "Rain") {
     drops.forEach(function(d) {
      d.drawAndDrop();
     });
   }
-
-// draw tree
-  push();
-    stroke(255);
-    translate(width/2, height-100);
-    let branch = new Branch(len, steps);
-    branch;
   pop();
 
+// draw mountain
+// push();
+// mountains.draw();
+// pop();
 
-// draw wind
+// draw tree
+  // push();
+  //   stroke(255);
+  //   translate(width/2, height-100);
+  //   let branch = new Branch(len, steps);
+  //   branch;
+  // pop();
+
+
+// draw wind 1 : circle
 push();
   windPosition.add(wind);
   stroke(0);
@@ -107,10 +122,13 @@ if (windPosition.x > width)  windPosition.x = 0;
 if (windPosition.x < 0)      windPosition.x = width;
 if (windPosition.y > height) windPosition.y = 0;
 if (windPosition.y < 0)      windPosition.y = height;
+
+// draw wind 2 : boids
+push();
+  flock.run();
+pop();
 }
 
-// draw wind 2
-// flock.run();
 
 // weather details
 function showWeather() {
@@ -177,6 +195,200 @@ function gotData(data) {
   currentTempreture = weather.main.temp;
   currentCity = weather.name;
 
-  let angle = radians(currentWindDeg);
+  windAngle = radians(currentWindDeg);
   wind = p5.Vector.fromAngle(angle);
+}
+
+function Boid(x,y) {
+
+  this.acceleration = createVector(0,0);
+  this.velocity =  createVector(random(-1,1),random(-1,1)); // currentWindSpeed
+  this.position = createVector(x,y);
+  this.r = 3.0;
+  this.maxspeed = 3;    // Maximum speed
+  this.maxforce = currentWindSpeed;//0.05; // Maximum steering force
+}
+
+Boid.prototype.run = function(boids) {
+  this.flock(boids);
+  this.update();
+  this.borders();
+  this.render();
+}
+
+Boid.prototype.applyForce = function(force) {
+  // We could add mass here if we want A = F / M
+  this.acceleration.add(force);
+}
+
+// We accumulate a new acceleration each time based on three rules
+
+// windPosition.add(wind);
+// this.velocity.add(this.acceleration);
+function Boid(x,y) {
+  // this.acceleration = createVector(0,0);
+  this.acceleration = wind.copy();
+  // this.velocity = createVector(random(-1,1),random(-1,1));
+  this.velocity = windPosition.copy();
+  this.position = createVector(x,y);
+  this.r = 3.0;
+  this.maxspeed = 3;    // Maximum speed
+  this.maxforce = 0.05; // Maximum steering force
+}
+
+Boid.prototype.run = function(boids) {
+  this.flock(boids);
+  this.update();
+  this.borders();
+  this.render();
+}
+
+Boid.prototype.applyForce = function(force) {
+  // We could add mass here if we want A = F / M
+  this.acceleration.add(force);
+}
+
+// We accumulate a new acceleration each time based on three rules
+Boid.prototype.flock = function(boids) {
+  var sep = this.separate(boids);   // Separation
+  var ali = this.align(boids);      // Alignment
+  var coh = this.cohesion(boids);   // Cohesion
+  // Arbitrarily weight these forces
+  sep.mult(1.5);
+  ali.mult(1.0);
+  coh.mult(1.0);
+  // Add the force vectors to acceleration
+  this.applyForce(sep);
+  this.applyForce(ali);
+  this.applyForce(coh);
+}
+
+
+// Method to update location
+Boid.prototype.update = function() {
+  // Update velocity
+  this.velocity.add(this.acceleration);
+  // Limit speed
+  this.velocity.limit(this.maxspeed);
+  this.position.add(this.velocity);
+  // Reset accelertion to 0 each cycle
+  this.acceleration.mult(0);
+}
+
+// A method that calculates and applies a steering force towards a target
+// STEER = DESIRED MINUS VELOCITY
+Boid.prototype.seek = function(target) {
+  var desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
+  // Normalize desired and scale to maximum speed
+  desired.normalize();
+  desired.mult(this.maxspeed);
+  // Steering = Desired minus Velocity
+  var steer = p5.Vector.sub(desired,this.velocity);
+  steer.limit(this.maxforce);  // Limit to maximum steering force
+  return steer;
+}
+
+Boid.prototype.render = function() {
+  // Draw a triangle rotated in the direction of velocity
+  var theta = this.velocity.heading() + radians(90);
+  fill(127);
+  stroke(200);
+  push();
+  translate(this.position.x,this.position.y);
+  rotate(theta);
+  beginShape();
+  vertex(0, -this.r*2);
+  vertex(-this.r, this.r*2);
+  vertex(this.r, this.r*2);
+  endShape(CLOSE);
+  pop();
+}
+
+// Wraparound
+Boid.prototype.borders = function() {
+  if (this.position.x < -this.r)  this.position.x = width +this.r;
+  if (this.position.y < -this.r)  this.position.y = height+this.r;
+  if (this.position.x > width +this.r) this.position.x = -this.r;
+  if (this.position.y > height+this.r) this.position.y = -this.r;
+}
+
+// Separation
+// Method checks for nearby boids and steers away
+Boid.prototype.separate = function(boids) {
+  var desiredseparation = 25.0;
+  var steer = createVector(0,0);
+  var count = 0;
+  // For every boid in the system, check if it's too close
+  for (var i = 0; i < boids.length; i++) {
+    var d = p5.Vector.dist(this.position,boids[i].position);
+    // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+    if ((d > 0) && (d < desiredseparation)) {
+      // Calculate vector pointing away from neighbor
+      var diff = p5.Vector.sub(this.position,boids[i].position);
+      diff.normalize();
+      diff.div(d);        // Weight by distance
+      steer.add(diff);
+      count++;            // Keep track of how many
+    }
+  }
+  // Average -- divide by how many
+  if (count > 0) {
+    steer.div(count);
+  }
+
+  // As long as the vector is greater than 0
+  if (steer.mag() > 0) {
+    // Implement Reynolds: Steering = Desired - Velocity
+    steer.normalize();
+    steer.mult(this.maxspeed);
+    steer.sub(this.velocity);
+    steer.limit(this.maxforce);
+  }
+  return steer;
+}
+
+// Alignment
+// For every nearby boid in the system, calculate the average velocity
+Boid.prototype.align = function(boids) {
+  var neighbordist = 50;
+  var sum = createVector(0,0);
+  var count = 0;
+  for (var i = 0; i < boids.length; i++) {
+    var d = p5.Vector.dist(this.position,boids[i].position);
+    if ((d > 0) && (d < neighbordist)) {
+      sum.add(boids[i].velocity);
+      count++;
+    }
+  }
+  if (count > 0) {
+    sum.div(count);
+    sum.normalize();
+    sum.mult(this.maxspeed);
+    var steer = p5.Vector.sub(sum,this.velocity);
+    steer.limit(this.maxforce);
+    return steer;
+  } else {
+    return createVector(0,0);
+  }
+}
+
+// Cohesion
+// For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
+Boid.prototype.cohesion = function(boids) {
+  var neighbordist = 50;
+  var sum = createVector(0,0);   // Start with empty vector to accumulate all locations
+  var count = 0;
+  for (var i = 0; i < boids.length; i++) {
+    var d = p5.Vector.dist(this.position,boids[i].position);
+    if ((d > 0) && (d < neighbordist)) {
+      sum.add(boids[i].position); // Add location
+      count++;
+    }
+  }
+  if (count > 0) {
+    sum.div(count);
+    return this.seek(sum);  // Steer towards the location
+  } else {
+    return createVector(0,0);
+  }
 }
